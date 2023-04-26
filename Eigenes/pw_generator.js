115ea5862a6c4f73e5d1564c80fe7a2b27f59420
ASK-USE <knueppelalex@web.de>
2023-04-26 22:33:53 +0200

    man kann noch arithmetische Operatoren eingeben..

--- Eigenes/pw_generator.js
+++ Eigenes/pw_generator.js
@@ -1,12 +1,8 @@
-// const signPool = {
-    // signs: "a","b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-    //          "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    //         "1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","-","_","=","<",">","?","/","[","]","{","}","|"};
-    const signPool = { 
-        signs: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
-        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", ")", "(", "-", "_", "=", "<", ">", "?", "/", 
-        "]", "[","}","{", "|"]
-      };
+const signPool = { 
+    signs: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", ")", "(", "-", "_", "=", "<", ">", "?", "/", 
+    "]", "[","}","{", "|"]
+};
        
     //   // Generiert zufällige Zahl zwischen 0 (einschließlich) und der Länge des signPool.signs-Arrays (und 1 (ausschließlich))      
     //     const randomIndex = Math.floor(Math.random() * signPool.signs.length);
@@ -17,12 +13,20 @@
     //      // Gibt ein zufälliges Element aus dem signPool-Array aus
     //     console.log(randomSign);
 
-        // Abfrage der gewünschten Passwort-Länge
-const passwordLength = parseInt(prompt("Geben Sie die gewünschte Länge des Passworts ein:"));
+        // Abfrage der gewünschten Passwort-Länge- leider auch negative Zahlen
+// const passwordLength = parseInt(prompt("character-length: "));
 
-// Festlegen der maximalen Passwort-Länge
-const maxLength = 200;
+// Abfrage der gewünschten Passwort-Länge - nur zahlen werden gewertet minus nicht.
+const passwordLength = Math.abs(parseInt(prompt("character-length: ")));
 
+if (passwordLength > 200) {
+    alert("uhh.. Bigboy!.. max. 200 characters permitted!");
+    passwordLength = 200;
+  } else if (passwordLength === 0) {
+    alert("at least 1 character needed!");
+  } else {
+        const maxLength = 200;// Festlegen der maximalen Passwort-Länge
+        const actualLength = Math.min(passwordLength, maxLength);
 // Generierung einer zufälligen Zeichenfolge aus dem signPool-Array
 let password = "";
 for (let i = 0; i < passwordLength && i < maxLength; i++) {
@@ -30,4 +34,5 @@ for (let i = 0; i < passwordLength && i < maxLength; i++) {
   password += signPool.signs[randomIndex];
 }
 // console.log(password); // Gibt das zufällige Passwort aus
-alert(password); // Gibt das zufällige Passwort in einer alert-box aus!
+// alert(password); // Gibt das zufällige Passwort in einer alert-box aus!
+alert(`Your new Password is: ${password}`);}
